c5d534eda4bbc26af6ebb859569c132950ba6f2d
M. Ali Naeem <Ali.Naeem@stallions.tech>
2024-11-12 18:24:45 +0500

    added animation

--- portfolio.client/src/app/about/about.component.ts
+++ portfolio.client/src/app/about/about.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { MatterJs } from './Matter';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
+  @ViewChild('matterContainer', { static: true }) matterContainer!: ElementRef;
 
+  ngOnInit(): void {
+    MatterJs.gyro(this.matterContainer.nativeElement, "images/html.png");
+  }
 }
