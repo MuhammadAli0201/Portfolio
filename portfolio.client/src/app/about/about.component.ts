f9da9c436eedbbb6f84700e106da602a5c19861e
M. Ali Naeem <Ali.Naeem@stallions.tech>
2024-11-15 15:31:22 +0500

    add slider to technology images

--- portfolio.client/src/app/about/about.component.ts
+++ portfolio.client/src/app/about/about.component.ts
@@ -1,12 +1,10 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { MatterJs } from './Matter';
 
-interface Image{
-  path:string,
-  tooltip:string,
-  tooltipColor:string,
-  height?:number,
-  width?:number
+interface Image {
+  path: string,
+  tooltip: string,
+  tooltipColor: string
 }
 
 @Component({
@@ -14,22 +12,52 @@ interface Image{
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
-export class AboutComponent implements OnInit {
+export class AboutComponent implements OnInit, AfterViewInit {
+
   @ViewChild('matterContainer', { static: true }) matterContainer!: ElementRef;
+  @ViewChild('techStack', { static: true }) techStack!: ElementRef;
+  displayImagesCount: number = 1;
+  startIndex: number = 0;
+  techImgWidth: number = 40;
+  techImgGap: number = 50;
 
   ngOnInit(): void {
     MatterJs.gyro(this.matterContainer.nativeElement, "images/html.png");
   }
 
+  ngAfterViewInit(): void {
+    Promise.resolve().then(() => {
+      let techStackDivWidth = this.techStack.nativeElement.clientWidth;
+      this.displayImagesCount = Math.ceil(techStackDivWidth / (this.techImgWidth + this.techImgGap));
+    });
+  }
+
   imagePaths: Image[] = [
-    {path:'images/dotnet.png',  tooltip:'.NET Core', tooltipColor:'purple'},
-    {path:'images/sql-server.png', tooltip:'SQL Server', tooltipColor:'red'},
-    {path:'images/angular.png', tooltip:'Angular', tooltipColor:'rgb(207, 10, 204)'},
-    {path:'images/ng zorro.png', tooltip:'NG Zorro', tooltipColor:'blue'},
-    {path:'images/html.png', tooltip:'HTML', tooltipColor:'orangered'},
-    {path: 'images/css.png', tooltip:'CSS', tooltipColor: 'blue'},
-    {path: 'images/js.png', tooltip: 'JavaScript', tooltipColor: 'rgb(196, 196, 25)'},
-    {path: 'images/flutter.png', tooltip:'Flutter', tooltipColor: 'skyblue'},
-    {path: 'images/firebase.png', tooltip: 'Firebase', tooltipColor: 'orange'},
+    { path: 'images/dotnet.png', tooltip: '.NET Core', tooltipColor: 'purple' },
+    { path: 'images/sql-server.png', tooltip: 'SQL Server', tooltipColor: 'red' },
+    { path: 'images/angular.png', tooltip: 'Angular', tooltipColor: 'rgb(207, 10, 204)' },
+    { path: 'images/ng zorro.png', tooltip: 'NG Zorro', tooltipColor: 'blue' },
+    { path: 'images/html.png', tooltip: 'HTML', tooltipColor: 'orangered' },
+    { path: 'images/css.png', tooltip: 'CSS', tooltipColor: 'blue' },
+    { path: 'images/js.png', tooltip: 'JavaScript', tooltipColor: 'rgb(196, 196, 25)' },
+    { path: 'images/flutter.png', tooltip: 'Flutter', tooltipColor: 'skyblue' },
+    { path: 'images/firebase.png', tooltip: 'Firebase', tooltipColor: 'orange' },
   ];
+
+  getVisibleImages = (): Image[] => this.imagePaths.slice(this.startIndex, this.displayImagesCount);
+  isLeftButtonVisible = (): boolean => this.startIndex > 0;
+  isRightButtonVisible = (): boolean => this.displayImagesCount < this.imagePaths.length;
+
+  slideImagesToLeft() {
+    if (this.startIndex > 0) {
+      this.startIndex--;
+      this.displayImagesCount--;
+    }
+  }
+  slideImagesToRight() {
+    if (this.displayImagesCount < this.imagePaths.length) {
+      this.startIndex++;
+      this.displayImagesCount++;
+    }
+  }
 }
